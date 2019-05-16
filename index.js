4a54588bf9c2bf45e10724645843cd343db1e81c
mariomui <mmui@riffyn.com>
2019-05-16 02:47:25 -0700

    Created both local and Codepen compatible index.html

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
-describe('hello', () => {
-  it("should equal 5", () => {
-    expect(5).toEqual(5);
+// write code below here 
+
+describe('switchy', ()=> {
+  it('should be a function', () => {
+    expect(typeof switchy).toEqual('function');
   })
-});
+  it('object should have all odd values', () => {
+    let arr = ['jack', 'bob'];
+    let obj = {};
+    function checkObj(obj) {
+      if (Object.keys(obj).length === 0) {
+        return false;
+      }
+      for (let key in obj) {
+        if ((obj[key] % 2) === 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+    switchy(arr,obj)
+    expect(checkObj(obj)).toEqual(true) 
+  })
+  
+  it("should have all the array values as object keys", () => {
+      let arr = ['bob','sam', 'jacky','smith'];
+      let obj = {}
+      function checkArr(arr) {  
+        for (var i = 0; i < arr.length; i++) {
+          if (obj[arr[i]] === undefined) {
+            return false;
+          }
+          return true;
+        }
+      }
+        switchy(arr,obj);
+        expect(checkArr(arr)).toEqual(true);
+      
+    });
+})
